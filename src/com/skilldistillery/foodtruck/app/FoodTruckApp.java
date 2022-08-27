5abbb059c8f0cfac78b50c2e3d83bb884fff1d83
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-08-26 21:35:55 -0600

    I cannot seem to figure out ow to properly work the FoodTruck [] into my loop. Will revisit in the morning.

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,62 @@
 package com.skilldistillery.foodtruck.app;
-
+import java.util.*;
+import com.skilldistillery.foodtruck.entities.*;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FoodTruckApp ourFoodTruckApp = new FoodTruckApp();
+//		FoodTruck[] trucks;
+		
+		ourFoodTruckApp.Welcome();
+		
+		Scanner scan = new Scanner(System.in);
+		String t1,t2;
+		int t3;
+		// Need to iterate this loop up to five times with quit exit to while loop.
+		int i = 0;
+		do {
+		System.out.print("Name: ");
+		t1 = scan.next();
+		if (t1.equalsIgnoreCase("quit")) {
+			ourFoodTruckApp.MainMenu();
+		}
+		System.out.println("Food Type: ");
+		t2 = scan.next();
+		System.out.println("Rating on scale 1 to 5 (Ate 1 and done, could easily eat 5-the best): ");
+		t3 = scan.nextInt();
+		FoodTruck trucks[i] = new FoodTruck(t1, t2, t3);
+		i++;
+		} while (i < 5);
+		
+		ourFoodTruckApp.MainMenu();
 
 	}
-
+public void Welcome() {
+	System.out.println("Welcome to the Food Truck Food Court Review App!");
+	System.out.println("Please input the name, food type and rating for up to 5 food trucks you visit today.");	
 }
+
+
+public void MainMenu() {
+	System.out.println("Thank you for your input. What would you like to do next? " );
+	System.out.println("1. List all existing food trucks." );
+	System.out.println("2. See the average rating of food trucks." );
+	System.out.println("3. Display the highest-rated food truck." );
+	System.out.println("4. Quit the program." );}
+}
+
+//public void calculateAverage(int[] rating) {
+//    double average = 0.0;
+//    
+//    // variable to hold the sum of all scores
+//    double total = 0.0;
+//    for (int i = 0; i < rating.length; i++) {
+//      total += rating[i];
+//    }
+//    
+//    average = sum / rating.length; // divide by the length because
+//                                   // we don't know how long the passed-in
+//                                   // array is (even though we're the ones passing it)
+//    return average;
+//  }
+
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,44 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+	private int numericID;
+	private String name;
+	private String foodType;
+	private double numericRating;
 
+
+public FoodTruck(String name, String foodType, double numericRating) {
+	
+}
+
+public 
+
+public String getName() {
+	return name;
+}
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+public String getFoodType() {
+	return foodType;
+}
+
+
+public void setFoodType(String foodType) {
+	this.foodType = foodType;
+}
+
+
+public double getNumericRating() {
+	return numericRating;
+}
+
+
+public void setNumericRating(double numericRating) {
+	this.numericRating = numericRating;
+}
 }
